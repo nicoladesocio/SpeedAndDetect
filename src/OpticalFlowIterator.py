9a63469b8be246b535b43c311b081db89f4ad3b5
nicoladesocio <nikdeso@hotmail.it>
2020-07-12 01:32:42 +0200

    written optical flow images to file

--- src/OpticalFlowIterator.py
+++ src/OpticalFlowIterator.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 class OpticalFlowIterator():
 	def __init__(self):
@@ -20,8 +21,16 @@ class OpticalFlowIterator():
 	def ComputeNextOpticalFlow(self):
 		self.video.set(1, self.frame_no)
 		ret, frame = self.video.read()
+		hsv = np.zeros_like(frame)
+		hsv[...,1] = 255
+		next = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+		prvs = cv2.cvtColor(self.images[-1],cv2.COLOR_BGR2GRAY)
+		flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+		mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+		hsv[...,0] = ang*180/np.pi/2
+		hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+		rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
 		self.images.append(frame)
 		self.frame_no = self.frame_no + 1
-		cv2.imshow('frame',frame)
-		cv2.waitKey(200)
+		cv2.imwrite("./out/training_" + str(self.frame_no) + ".jpg", rgb)
 		return
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import OpticalFlowIterator as ofi
 def main():
 	optical_flows = ofi.OpticalFlowIterator()
 	optical_flows.Import("../speedchallenge/data/train.mp4")
-	for i in range(0,50):
+	for i in range(0,500):
 		optical_flows.ComputeNextOpticalFlow()
 	print("hello world!")
 
