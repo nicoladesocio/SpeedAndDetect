6baba8d2bc3b4f368f440058eb80b6e878b4e1a4
nicoladesocio <nikdeso@hotmail.it>
2020-07-11 18:30:23 +0200

    Added capture reading and wait for 200 ms after read
    Added 50 frames reading test in main

--- src/OpticalFlowIterator.py
+++ src/OpticalFlowIterator.py
@@ -1,14 +1,27 @@
+import cv2
+
 class OpticalFlowIterator():
 	def __init__(self):
 		self.images = []
 		self.optical_flows = []
 		self.mp4_path = ""
+		self.video = None
+		self.frame_no = 0
 
 	def Import(self, mp4_path):
 		self.mp4_path = mp4_path
-		#implement
+		self.video = cv2.VideoCapture(mp4_path)
+		ret, frame = self.video.read()
+		# ignore the error for now
+		self.images.append(frame)
+		self.frame_no = self.frame_no + 1 
 		return
 
-	def ComputeNextOpticalFlow():
-		#implement
+	def ComputeNextOpticalFlow(self):
+		self.video.set(1, self.frame_no)
+		ret, frame = self.video.read()
+		self.images.append(frame)
+		self.frame_no = self.frame_no + 1
+		cv2.imshow('frame',frame)
+		cv2.waitKey(200)
 		return
