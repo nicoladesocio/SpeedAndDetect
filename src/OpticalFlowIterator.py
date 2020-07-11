741695021c7d9e73de7bdb44369106a3435b36ca
nicoladesocio <nikdeso@hotmail.it>
2020-07-11 13:37:16 +0200

    added video capture code
    not tested

--- src/OpticalFlowIterator.py
+++ src/OpticalFlowIterator.py
@@ -1,3 +1,5 @@
+import cv2
+
 class OpticalFlowIterator():
 	def __init__(self):
 		self.images = []
@@ -6,9 +8,29 @@ class OpticalFlowIterator():
 
 	def Import(self, mp4_path):
 		self.mp4_path = mp4_path
-		#implement
+		cap = cv2.VideoCapture(mp4_path)
+		# Check if camera opened successfully
+		if (cap.isOpened()== False): 
+  			print("Error opening video stream or file")
+		# Read until video is completed
+		while(cap.isOpened()):
+  			# Capture frame-by-frame
+			ret, frame = cap.read()
+			if ret == True:
+    			# Display the resulting frame
+				cv2.imshow('Frame',frame)
+    			# Press Q on keyboard to  exit
+				if cv2.waitKey(25) & 0xFF == ord('q'):
+					break
+  			# Break the loop
+			else: 
+				break
+		# When everything done, release the video capture object
+		cap.release()
+		# Closes all the frames
+		cv2.destroyAllWindows()
 		return
 
 	def ComputeNextOpticalFlow():
-		#implement
+		# implement
 		return
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import OpticalFlowIterator as ofi
 
+
 def main():
 	optical_flows = ofi.OpticalFlowIterator()
+	optical_flows.Import("../speedchallenge/data/test.mp4")
 	print("hello world!")
 
 if __name__ == "__main__":
